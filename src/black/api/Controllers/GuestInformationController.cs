bc214d7a26aec1672611f0078566af6e816c582c
Thomas Ziemek <tziemek@artiso.com>
2020-10-13 17:34:05 +0200

    change target framework to netstandard2.0 and netcoreapp3.1

--- src/black/api/Controllers/GuestInformationController.cs
+++ src/black/api/Controllers/GuestInformationController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using artiso.AdsdHotel.Black.Api;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
