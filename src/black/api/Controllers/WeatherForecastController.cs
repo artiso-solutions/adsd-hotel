596f699ce8d80b8d9a68fa557b1114301fc33c9d
Thomas Ziemek <tziemek@artiso.com>
2020-10-15 10:24:38 +0200

    restructure folders; switch to netcoreapp3.1 and sdk 3.1.*

--- src/black/api/Controllers/WeatherForecastController.cs
+++ src/black/api/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace artiso.AdsdHotel.Black.GuestInformation.Api.Controllers
+namespace artiso.AdsdHotel.Black.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
