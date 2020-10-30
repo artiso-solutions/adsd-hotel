644838a4a0643dcbaa1498684403e76d3d25b875
Tobias Tengler <ttengler@artiso.com>
2020-10-30 16:13:31 +0100

    Add contracts

---
+++ services/blue/src/contracts/PendingReservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace artiso.AdsdHotel.Blue.Contracts
+{
+    public class PendingReservation
+    {
+        internal PendingReservation(string id, string orderId, string roomTypeId, DateTime start, DateTime end, DateTime createdAt)
+        {
+            Id = id;
+            OrderId = orderId;
+            RoomTypeId = roomTypeId;
+            Start = start;
+            End = end;
+            CreatedAt = createdAt;
+        }
+
+        public string Id { get; }
+
+        public string OrderId { get; }
+
+        public string RoomTypeId { get; }
+
+        public DateTime Start { get; }
+
+        public DateTime End { get; }
+
+        public DateTime CreatedAt { get; }
+    }
+}
---
+++ services/blue/src/contracts/RoomType.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace artiso.AdsdHotel.Blue.Contracts
+{
+    public class RoomType
+    {
+        internal RoomType(string id, string name, int capacity, IReadOnlyList<BedType> bedTypes)
+        {
+            Id = id;
+            Name = name;
+            Capacity = capacity;
+            BedTypes = bedTypes;
+        }
+
+        public string Id { get; }
+
+        public string Name { get; }
+
+        public int Capacity { get; }
+
+        public IReadOnlyList<BedType> BedTypes { get; }
+    }
+}
