01f5aff5a29cd6ac0538eaa8d2470df681439ec5
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-11-09 20:44:19 +0100

    Async support on IDbCommand and IDbTransaction.

---
+++ services/blue/src/api/DatabaseAccess/DatabaseExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace artiso.AdsdHotel.Blue.Api
+{
+    internal static class DatabaseExtensions
+    {
+        public static async Task CommitAsync(this IDbTransaction transaction)
+        {
+            if (transaction is DbTransaction dbTransaction)
+            {
+                await dbTransaction.CommitAsync();
+            }
+            else
+            {
+                transaction.Commit();
+            }
+        }
+
+        public static async Task RollbackAsync(this IDbTransaction transaction)
+        {
+            if (transaction is DbTransaction dbTransaction)
+            {
+                await dbTransaction.RollbackAsync();
+            }
+            else
+            {
+                transaction.Rollback();
+            }
+        }
+
+        public static async Task ExecuteNonQueryAsync(this IDbCommand command)
+        {
+            if (command is DbCommand dbCommand)
+            {
+                await dbCommand.ExecuteNonQueryAsync();
+            }
+            else
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
