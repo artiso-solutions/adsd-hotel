37410ed7101f320486bd4f728f5233c70560eac0
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-11-10 11:41:54 +0100

    Introduces query models and reads available room types.

--- services/blue/src/api/Handlers/AvailabilityHandler.cs
+++ services/blue/src/api/Handlers/AvailabilityHandler.cs
@@ -1,7 +1,8 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 using artiso.AdsdHotel.Blue.Commands;
-using artiso.AdsdHotel.Blue.Contracts;
 using NServiceBus;
 using RepoDb;
 using static artiso.AdsdHotel.Blue.Api.DatabaseTableNames;
@@ -22,16 +23,62 @@ namespace artiso.AdsdHotel.Blue.Api
             await using var connection = await _connectionFactory.CreateAsync();
 
             var query = $@"
-SELECT * FROM {V_RoomTypes} AS vrt
+SELECT Id, InternalName, Capacity, `BedType.Id`, `BedType.InternalName`, `Width`, `Length`
+FROM {V_RoomTypes} AS vrt
 WHERE vrt.Id NOT IN (
-	SELECT DISTINCT RoomTypeId FROM Reservations
+	SELECT DISTINCT RoomTypeId FROM {Reservations}
 	WHERE Start >= @Start AND Start <= @End)";
 
-            var queryResult = await connection.ExecuteQueryAsync<RoomType>(query, new { message.Start, message.End });
+            using var reader = await connection.ExecuteReaderAsync(query, new { message.Start, message.End });
 
-            var availableRoomTypes = queryResult.ToArray();
+            var queryModels = ReadQueryModels(reader);
+            var availableRoomTypes = queryModels.Select(QueryModelMapper.Map).ToList();
 
             await context.Reply(new AvailableRoomTypesResponse { RoomTypes = availableRoomTypes });
+        }
+
+        private IReadOnlyList<RoomTypeQueryModel> ReadQueryModels(IDataReader reader)
+        {
+            var stash = new Dictionary<string, RoomTypeQueryModel>();
+
+            while (reader.Read())
+            {
+                var roomTypeQueryModel = ReadRoomTypeIfNew();
+
+                var bedTypeQueryModel = new BedTypeQueryModel
+                {
+                    Id = reader.GetString(4),
+                    InternalName = reader.GetString(5),
+                    Width = reader.GetDouble(6),
+                    Length = reader.GetDouble(7),
+                };
+
+                roomTypeQueryModel.BedTypes.Add(bedTypeQueryModel);
+            }
+
+            var queryModels = stash.Values.ToArray();
+            return queryModels;
+
+            // Local functions.
+
+            RoomTypeQueryModel ReadRoomTypeIfNew()
+            {
+                var id = reader.GetString(0);
+
+                if (!stash.TryGetValue(id, out var roomTypeQueryModel))
+                {
+                    roomTypeQueryModel = new RoomTypeQueryModel
+                    {
+                        Id = id,
+                        InternalName = reader.GetString(1),
+                        Capacity = reader.GetInt32(2)
+                    };
+
+                    stash.Add(id, roomTypeQueryModel);
+                }
+
+                return roomTypeQueryModel;
+            }
         }
     }
 }
---
+++ services/blue/src/api/QueryModels/RoomTypeQueryModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace artiso.AdsdHotel.Blue.Api
+{
+    internal class RoomTypeQueryModel
+    {
+        public string? Id { get; set; }
+
+        public string? InternalName { get; set; }
+
+        public int? Capacity { get; set; }
+
+        public List<BedTypeQueryModel> BedTypes { get; set; } = new List<BedTypeQueryModel>();
+    }
+}
