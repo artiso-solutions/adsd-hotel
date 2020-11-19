141a400a11c9bffe0e2591115aad90d1b1322fe0
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-11-19 10:38:51 +0100

    V_Reservations also contains the room Id.

--- services/blue/src/api/DatabaseAccess/Migrations/05.ViewReservations.cs
+++ services/blue/src/api/DatabaseAccess/Migrations/05.ViewReservations.cs
@@ -10,7 +10,7 @@ namespace artiso.AdsdHotel.Blue.Api
         {
             var statement = $@"
 CREATE VIEW `{V_Reservations}` AS
-SELECT res.OrderId, rs.Number as `RoomNumber`, rt.InternalName as `RoomType`, res.Start, res.End
+SELECT res.OrderId, rs.Id as `RoomId`, rs.Number as `RoomNumber`, rt.InternalName as `RoomType`, res.Start, res.End
 FROM {Reservations} res
 INNER JOIN {Rooms} rs ON rs.Id = res.RoomId
 INNER JOIN {RoomTypes} rt ON rt.Id = res.RoomTypeId";
