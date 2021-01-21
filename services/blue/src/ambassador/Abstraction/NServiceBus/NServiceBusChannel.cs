0c1cafe13c316fd48f73a0d485f325af3587405b
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2021-01-21 17:10:05 +0100

    Introduces IChannel abstraction for ambassadors.

---
+++ services/blue/src/ambassador/Abstraction/NServiceBus/NServiceBusChannel.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace artiso.AdsdHotel.Blue.Ambassador
+{
+    public class NServiceBusChannel : IChannel
+    {
+        private readonly EndpointHolder _holder;
+        private readonly string _destination;
+
+        public NServiceBusChannel(
+            EndpointHolder holder, string destination)
+        {
+            _holder = holder;
+            _destination = destination;
+        }
+
+        public async Task Send(object command)
+        {
+            await _holder.EndpointReady.ConfigureAwait(false);
+            await _holder.Endpoint.Send(_destination, command).ConfigureAwait(false);
+        }
+
+        public async Task<TResponse> Request<TResponse>(object request, CancellationToken cancellationToken)
+        {
+            await _holder.EndpointReady.ConfigureAwait(false);
+            var response = await _holder.Endpoint.Request<TResponse>(request, cancellationToken).ConfigureAwait(false);
+            return response;
+        }
+
+        public async Task Publish(object evt)
+        {
+            await _holder.EndpointReady.ConfigureAwait(false);
+            await _holder.Endpoint.Publish(evt).ConfigureAwait(false);
+        }
+
+        public ValueTask DisposeAsync() =>
+            _holder.DisposeAsync();
+    }
+}
---
+++ services/blue/src/ambassador/Abstraction/NServiceBus/NServiceBusChannelFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NServiceBus;
+
+namespace artiso.AdsdHotel.Blue.Ambassador
+{
+    public class NServiceBusChannelFactory
+    {
+        public static IChannel Create(
+            string channelName,
+            string endpointDestination,
+            string rabbitMqConnectionString)
+        {
+            var config = new EndpointConfiguration(channelName);
+            config.MakeInstanceUniquelyAddressable($"{channelName}.{Guid.NewGuid()}");
+
+            NServiceBusEndpointConfigurator.Configure(config);
+
+            config.UseTransport<RabbitMQTransport>()
+                .UseConventionalRoutingTopology()
+                .ConnectionString(rabbitMqConnectionString);
+
+            var holder = new EndpointHolder(Endpoint.Start(config));
+            var channel = new NServiceBusChannel(holder, endpointDestination);
+
+            return channel;
+        }
+    }
+}
