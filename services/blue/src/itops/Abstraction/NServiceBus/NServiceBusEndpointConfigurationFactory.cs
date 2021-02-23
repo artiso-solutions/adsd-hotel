1127a28bf4c27bdac75d49e56a74c8662119ab6b
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2021-02-23 11:56:25 +0100

    Introduces BlackClientFactory.
    - use of shared itops project: artiso.AdsdHotel.ITOps
    - use of IMessageConvention
    - provides already-initialized BlackClient

--- services/blue/src/itops/Abstraction/NServiceBus/NServiceBusEndpointConfigurationFactory.cs
+++ services/blue/src/itops/Abstraction/NServiceBus/NServiceBusEndpointConfigurationFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using artiso.AdsdHotel.ITOps.Communication;
 using NServiceBus;
 
 namespace artiso.AdsdHotel.ITOps.Abstraction.NServiceBus
@@ -31,30 +30,7 @@ namespace artiso.AdsdHotel.ITOps.Abstraction.NServiceBus
             config.UsePersistence<InMemoryPersistence>();
 
             config.Conventions()
-                .DefiningCommandsAs(t => IsACommand(t) && !IsGenericResponse(t))
-                .DefiningMessagesAs(t => IsAResponse(t) || IsGenericResponse(t))
-                .DefiningEventsAs(IsAnEvent);
-
-            static bool IsACommand(Type t) =>
-                t.Namespace is not null &&
-                t.Namespace.EndsWith(".Commands") &&
-                !t.Name.EndsWith("Response");
-
-            static bool IsAResponse(Type t) =>
-                t.Namespace is not null &&
-                t.Namespace.EndsWith(".Commands") &&
-                t.Name.EndsWith("Response");
-
-            static bool IsGenericResponse(Type t)
-            {
-                if (!t.IsGenericType) return false;
-                var generic = t.GetGenericTypeDefinition();
-                return generic == typeof(Response<>);
-            }
-
-            static bool IsAnEvent(Type t) =>
-                t.Namespace is not null &&
-                t.Namespace.EndsWith(".Events");
+                .Add(new AdsdHotelMessageConventions());
         }
 
         private static void ConfigureRabbitMQ(EndpointConfiguration config, string rabbitMqConnectionString)
