6fee6774575b1db58c77e146e989578223fd1c7f
Thomas Ziemek <thomas.ziemek@gmail.com>
2021-07-29 16:38:03 +0200

    ToDo

--- services/yellow/src/api/Handlers/AddPaymentMethodToOrderHandler.cs
+++ services/yellow/src/api/Handlers/AddPaymentMethodToOrderHandler.cs
@@ -28,39 +28,35 @@ namespace artiso.AdsdHotel.Yellow.Api.Handlers
 
         public async Task Handle(AddPaymentMethodToOrderRequest message, IMessageHandlerContext context)
         {
-            try
-            {
-                _logger.LogInformation($"Received message for orderId: '{message.OrderId}'");
-                var validateResult = ValidateRequest(message);
-                if (!validateResult.IsValid())
-                    throw new ValidationException(validateResult.GetErrors());
-            
-                var order = await _orderService.FindOneById(message.OrderId);
-            
-                var paymentToken = await _paymentService.GetPaymentToken(message.PaymentMethod.CreditCard!);
-                var orderPaymentMethod = new StoredPaymentMethod(message.PaymentMethod.CreditCard!.GetOrderCreditCard(paymentToken));
-            
-                await _orderService.AddPaymentMethod(order, orderPaymentMethod);
-            
-                await context.Publish(new PaymentMethodToOrderAdded(message.OrderId));
-                await context.Reply(new Response<bool>(true));
-                _logger.LogInformation($"Finished {nameof(AddPaymentMethodToOrderHandler)} for orderId: '{message.OrderId}'");
-            }
-            catch (ValidationException e)
-            {
-                await context.Publish(new AddPaymentMethodToOrderFailed(message.OrderId, e.Message));
-                await context.Reply(new Response<bool>(e));
-            }
+            // ToDo maybe look into message headers if we can decide if the sender is requesting or sending
+            //context.ReplyToAddress
+            _logger.LogInformation($"Received message for orderId: '{message.OrderId}'");
+            var validateResult = ValidateRequest(message);
+            if (!validateResult.IsValid())
+                throw new ValidationException(validateResult.GetErrors());
+
+            var order = await _orderService.FindOneById(message.OrderId);
+
+            var paymentToken = await _paymentService.GetPaymentToken(message.PaymentMethod.CreditCard!);
+            var orderPaymentMethod = new StoredPaymentMethod(message.PaymentMethod.CreditCard!.GetOrderCreditCard(paymentToken));
+
+            await _orderService.AddPaymentMethod(order, orderPaymentMethod);
+
+            await context.Publish(new PaymentMethodToOrderAdded(message.OrderId));
+            _logger.LogInformation($"Finished {nameof(AddPaymentMethodToOrderHandler)} for orderId: '{message.OrderId}'");
+
+            // ToDo
+            //await context.Publish(new AddPaymentMethodToOrderFailed(message.OrderId, e.Message));
         }
 
         private ValidationModelResult<AddPaymentMethodToOrderRequest> ValidateRequest(AddPaymentMethodToOrderRequest message)
         {
             return message.Validate()
-                .HasData(r => r.OrderId, 
+                .HasData(r => r.OrderId,
                     $"{nameof(AddPaymentMethodToOrderRequest.OrderId)} should contain data")
-                .NotNull(r => r.PaymentMethod, 
+                .NotNull(r => r.PaymentMethod,
                     $"{nameof(AddPaymentMethodToOrderRequest.PaymentMethod)} should not be null")
-                .NotNull(r => r.PaymentMethod.CreditCard, 
+                .NotNull(r => r.PaymentMethod.CreditCard,
                     $"{nameof(AddPaymentMethodToOrderRequest.PaymentMethod.CreditCard)} should not be null")
                 .PaymentMethodIsValid(r => r.PaymentMethod);
         }
